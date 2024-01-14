3f4f95849188d8312dffaabe7524f9a29fc9e337
Hudaaaaaaaaaaa <156574699+Hudaaaaaaaaaaa@users.noreply.github.com>
2024-01-14 14:55:06 +0500

    Add files via upload

---
+++ huda.py
@@ -0,0 +1,14 @@
+opr=str(input("enter any operator"))
+number1=(int(input("Enter any number1 ")))
+number2=(int(input("Enter any number2 ")))
+if opr == "+":
+    print(number1 + number2)
+elif opr== "-":
+    print(number1 - number2)
+elif opr == "*":
+    print(number1 * number2)
+elif opr=="/":
+    print(number1 / number2)
+else:
+    print("Invalid operator")
+
